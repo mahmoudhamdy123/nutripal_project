efe028c3e0adced60cbbcfb200b1663826d160a9
Mahmoud Hamdy <mh2276419@gmail.com>
2019-08-23 17:49:14 +0200

    landscape&portrait

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nutripal_project/landscape/start_screen_landscape.dart';
+import 'package:nutripal_project/portrait/start_screen_portrait.dart';
 
 class StartScreen extends StatefulWidget {
   @override
@@ -9,131 +11,9 @@ class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/img4.jpeg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-                flex: 15,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'nutripal',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 80,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Pacifico',
-                      ),
-                    ),
-                    Text(
-                      'Variety.convenience.Delivered.',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                )),
-            Expanded(
-                flex: 5,
-                child: Column(
-              children: <Widget>[
-                Container(
-                  width:
-                  MediaQuery.of(context).orientation == Orientation.portrait
-                      ? MediaQuery.of(context).size.width * 0.9
-                      : MediaQuery.of(context).size.width * 0.7,
-                  height:
-                  MediaQuery.of(context).orientation == Orientation.portrait
-                      ? MediaQuery.of(context).size.width * 0.13
-                      : MediaQuery.of(context).size.width * 0.08,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(0, 230, 230, 1),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(top: 20)),
-                Container(
-                  width:
-                  MediaQuery.of(context).orientation == Orientation.portrait
-                      ? MediaQuery.of(context).size.width * 0.9
-                      : MediaQuery.of(context).size.width * 0.7,
-                  height:
-                  MediaQuery.of(context).orientation == Orientation.portrait
-                      ? MediaQuery.of(context).size.width * 0.13
-                      : MediaQuery.of(context).size.width * 0.08,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Text(
-                    'Log in',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Text(
-                        'Already have an account?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: BorderDirectional(
-                              bottom: BorderSide(
-                                  color: Color.fromRGBO(0, 230, 230, 1),
-                                  width: 0.8))),
-                      child: Text(
-                        'Login',
-
-                        style: TextStyle(
-                          color: Color.fromRGBO(0, 230, 230, 1),
-                          fontSize: 17,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ))
-          ],
-        ),
-      ),
+      body: MediaQuery.of(context).orientation == Orientation.portrait?
+      StartScreenPortrait():
+      StartScreenLandscape(),
     );
   }
 }
