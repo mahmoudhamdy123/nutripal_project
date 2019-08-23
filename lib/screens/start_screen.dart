2504a1022ac711abec86bd9e15bd2ab8603bec86
Rami ahmed <35673698+ramisadek@users.noreply.github.com>
2019-08-23 14:51:05 +0200

    delete all the expanded and comment on alligment

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -20,112 +20,101 @@ class _StartScreenState extends State<StartScreen> {
         ),
         child: Center(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            /// here we use the alligment cuz al coloumn mesh bia3tarf b
+            /// ae no3 men al alligment fo2eh 7ata law kan center class
             children: <Widget>[
-              Expanded(
-                flex: 2,
+              Text(
+                'nutripal',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 80,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  backgroundColor: Colors.green,
+                ),
+              ),
+              Text(
+                'Variety.convenience.Delivered.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width*0.9,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(0, 230, 230, 1),
+                  borderRadius: BorderRadius.circular(30),
+                ),
                 child: Text(
-                  'nutripal',
+                  'Sign up',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 80,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                    backgroundColor: Colors.green,
                   ),
                 ),
               ),
-              Expanded(
-                flex: 2,
+              Container(
+                width: MediaQuery.of(context).size.width*0.9,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.white,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
                 child: Text(
-                  'Variety.convenience.Delivered.',
+                  'Log in',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 21,
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  width: MediaQuery.of(context).size.width*0.9,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(0, 230, 230, 1),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  width: MediaQuery.of(context).size.width*0.9,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Text(
-                    'Log in',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(
-                          top: MediaQuery.of(context).padding.left + 10,
-                        ),
-                        child: Text(
-                          'Already have an account?',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.left + 10,
+                      ),
+                      child: Text(
+                        'Already have an account?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
                         ),
                       ),
                     ),
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.only(
-                          top: MediaQuery.of(context).padding.left + 10,
-                        ),
-                        decoration: BoxDecoration(
-                            border: BorderDirectional(
-                                bottom: BorderSide(
-                                    color: Color.fromRGBO(0, 230, 230, 1),
-                                    width: 0.8))),
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            color: Color.fromRGBO(0, 230, 230, 1),
-                            fontSize: 17,
-                          ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.left + 10,
+                      ),
+                      decoration: BoxDecoration(
+                          border: BorderDirectional(
+                              bottom: BorderSide(
+                                  color: Color.fromRGBO(0, 230, 230, 1),
+                                  width: 0.8))),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Color.fromRGBO(0, 230, 230, 1),
+                          fontSize: 17,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
